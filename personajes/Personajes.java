6e173ef35e3f7488996e097f8b176de9c45b1314
doueduar <doueduar12@gmail.com>
2021-03-14 14:31:44 -0600

    Personaje Bestia y Heroe

---
+++ personajes/Personajes.java
@@ -0,0 +1,21 @@
+package personajes;
+
+/**
+ * Personajes
+ */
+public class Personajes {
+    protected int vida;
+    protected int armadura;
+    protected int ataque;
+    protected String nombre;
+    protected boolean esHeroe;
+
+    private static int instanciasPersonajes;
+
+    public Personajes(int vida, int armadura, int ataque, String nombre, boolean esHeroe) {
+
+        
+    }
+    
+}
+
